7a39b2b1d4a289d14e5432855e017ca61c91f28d
Kolesza Renáta <kolreny@gmail.com>
2020-04-09 17:01:13 +0200

    created project

---
+++ TodoList/app/src/main/java/hu/bme/aut/todolist/MainActivity.java
@@ -0,0 +1,14 @@
+package hu.bme.aut.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
