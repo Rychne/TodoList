f86f5f4d220917dded75a301fc5353e6de4517c5
Kolesza Renáta <kolreny@gmail.com>
2020-04-14 10:55:06 +0200

    created model classes

---
+++ TodoList/app/src/main/java/hu/bme/aut/todolist/model/Tasks.java
@@ -0,0 +1,62 @@
+package hu.bme.aut.todolist.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tasks {
+
+    private List<Item> items = new ArrayList<>();
+    private Integer limit;
+    private String next;
+    private Integer offset;
+    private Object previous;
+    private Integer total;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    public Object getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Object previous) {
+        this.previous = previous;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
---
+++ TodoList/app/src/main/java/hu/bme/aut/todolist/model/TasksResult.java
@@ -0,0 +1,14 @@
+package hu.bme.aut.todolist.model;
+
+public class TasksResult {
+
+    private Tasks tasks;
+
+    public Tasks getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Tasks tasks) {
+        this.tasks = tasks;
+    }
+}
