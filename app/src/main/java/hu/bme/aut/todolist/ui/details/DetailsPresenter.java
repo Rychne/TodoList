1da12ddba132cb77b8b3a642bd2ccc690f0dfd7b
Kolesza Renáta <kolreny@gmail.com>
2020-04-19 09:57:41 +0200

    event bus for communication

--- app/src/main/java/hu/bme/aut/todolist/ui/details/DetailsPresenter.java
+++ app/src/main/java/hu/bme/aut/todolist/ui/details/DetailsPresenter.java
@@ -1,8 +1,12 @@
 package hu.bme.aut.todolist.ui.details;
 
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import javax.inject.Inject;
 
 import hu.bme.aut.todolist.interactor.details.DetailsInteractor;
+import hu.bme.aut.todolist.interactor.details.GetTaskDetailsEvent;
 import hu.bme.aut.todolist.ui.Presenter;
 
 public class DetailsPresenter extends Presenter<DetailsScreen> {
@@ -26,5 +30,10 @@ public class DetailsPresenter extends Presenter<DetailsScreen> {
 
     public void refreshTaskDetails(final String taskDetailsQuery) {
         detailsInteractor.getTaskDetails(taskDetailsQuery);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEventMainThread(final GetTaskDetailsEvent event) {
+        screen.showTask(event.getTaskDetails());
     }
 }
--- app/src/main/java/hu/bme/aut/todolist/ui/main/MainPresenter.java
+++ app/src/main/java/hu/bme/aut/todolist/ui/main/MainPresenter.java
@@ -1,7 +1,11 @@
 package hu.bme.aut.todolist.ui.main;
 
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import javax.inject.Inject;
 
+import hu.bme.aut.todolist.interactor.tasks.GetTasksEvent;
 import hu.bme.aut.todolist.interactor.tasks.TasksInteractor;
 import hu.bme.aut.todolist.ui.Presenter;
 
@@ -25,5 +29,10 @@ public class MainPresenter extends Presenter<MainScreen> {
 
     public void refreshTasks() {
         tasksInteractor.getTasks();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEventMainThread(final GetTasksEvent event) {
+        screen.showTasks(event.getTasks());
     }
 }
