55c7c3da0c70fbdc9c5984874275e5d793705e1c
Rychne <kolreny@gmail.com>
2020-04-25 11:54:05 +0200

    Network module

---
+++ app/src/main/java/hu/bme/aut/todolist/network/NetworkModule.java
@@ -0,0 +1,28 @@
+package hu.bme.aut.todolist.network;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+public class NetworkModule {
+
+    @Provides
+    @Singleton
+    public Retrofit.Builder provideRetrofit() {
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create());
+
+    }
+
+    @Provides
+    @Singleton
+    public TaskApi provideArtistsApi(Retrofit.Builder retrofitBuilder) {
+        return retrofitBuilder.baseUrl(NetworkConfig.ENDPOINT_ADDRESS).build().create(TaskApi.class);
+    }
+
+
+}
